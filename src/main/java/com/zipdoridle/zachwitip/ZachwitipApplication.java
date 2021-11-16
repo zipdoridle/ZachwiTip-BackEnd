9e7cfa54b4745edafdda6dd7c39dfd2a72aefcae
YechanJin <roqhrryghl@gmail.com>
2021-11-16 20:07:53 +0900

    Initial Commit

---
+++ src/main/java/com/zipdoridle/zachwitip/ZachwitipApplication.java
@@ -0,0 +1,13 @@
+package com.zipdoridle.zachwitip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZachwitipApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZachwitipApplication.class, args);
+    }
+
+}
