55925320b053153e8239f9a8d6520196525bd59d
YechanJin <roqhrryghl@gmail.com>
2021-11-19 23:53:46 +0900

    feat: spring security 의존성 추가, securityconfig 추가

---
+++ src/main/java/com/zipdoridle/zachwitip/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.zipdoridle.zachwitip.config;
+
+import com.zipdoridle.zachwitip.auth.service.CustomOAuth2UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/", "/images/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .logout()
+                .invalidateHttpSession(true).deleteCookies("JSESSIONID")
+                .logoutSuccessUrl("/")
+                .and()
+                .oauth2Login()
+                .userInfoEndpoint()
+                .userService(customOAuth2UserService);
+    }
+}
